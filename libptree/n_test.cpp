93597ae45b2dfc27bb04da5e5edb4da0b141d451
mencak <mencak>
2006-05-16 11:37:15 +0000

    Implementing TEST action.

---
+++ libptree/n_test.cpp
@@ -0,0 +1,88 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef DG_DIAGNOSE
+#include "diagnose/dg.h"
+#endif
+
+#include "n.h"                  /* Node, nTest, ... */
+
+#include "constants.h"
+#include "i18.h"
+#include "sysdep.h"             /* BOOL, STD_BUF, ... */
+
+#include "free.h"               /* FREE(), DELETE() */
+#include "io.h"                 /* file_ropen(), ... */
+#include "str.h"
+
+#include <signal.h>             /* signal() */
+#include <stdlib.h>             /* exit(), system() */
+#include <stdio.h>              /* stderr */
+#include <errno.h>              /* errno */
+
+#include <iostream>             /* std::string, cout, endl, ... */
+#include <sstream>              /* ostringstream */
+
+using namespace std;
+
+/*
+ * nTest constructor
+ */
+nTest::nTest()
+{
+  /* initialisation */
+  init();
+}
+
+/*
+ * Initialise nTest request
+ */
+void
+nTest::init()
+{
+  expr = NULL;
+}
+
+/*
+ * nTest copy constuctor
+ */
+nTest::nTest(Node &node)
+{
+  Node::init(node);
+  init();
+}
+
+/*
+ * nTest destructor
+ */
+nTest::~nTest()
+{
+  DELETE(expr);
+}
+
+int
+nTest::exec(Process *proc)
+{
+  DM_DBG_I;
+
+  int rval = 0;
+
+  std::string s = Process::eval_str(expr, proc);
+
+  if(s.length() == 0 || (s.length() == 1 && s[0] == 0)) return ERR_ERR;
+
+  RETURN(ERR_OK);
+}
+
+std::string
+nTest::toString(Process *proc)
+{
+  BOOL quote = TRUE;
+  std::stringstream ss;
+
+  ss << "TEST";
+  SS_DQ(" ", expr);
+
+  return ss.str();
+}

74de410fc8498dc8e539ada3b0af32356ce5776d
mencak <mencak>
2006-07-03 15:04:22 +0000

    Adding a forgotten SRM 2.2 method.

---
+++ protos/srm/2.2/api/ResumeRequest.cpp
@@ -0,0 +1,62 @@
+/**
+ * \file ResumeRequest.cpp
+ *
+ * Implements the SRM2 ResumeRequest method.  SRM2 spec p.21.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef DG_DIAGNOSE
+#include "diagnose/dg.h"
+#endif
+
+#include "gsoap/soapH.h"
+
+#ifdef HAVE_CGSI_PLUGIN
+#include "cgsi_plugin.h"
+#endif
+
+#include "srm2api.h"
+#include "srm_macros.h"
+
+/**
+ * srmResumeRequest method.
+ *
+ * \param soap
+ * \param srm_endpoint
+ * \param authorizationID
+ * \param requestToken
+ * \param resp request response
+ *
+ * \returns request exit status (EXIT_SUCCESS/EXIT_FAILURE)
+ */
+extern int
+ResumeRequest(struct soap *soap,
+              const char *srm_endpoint,
+              const char *authorizationID,
+              const char *requestToken,
+              struct srm__srmResumeRequestResponse_ *resp)
+{
+  DM_DBG_I;
+  struct srm__srmResumeRequestRequest req;
+
+  SOAP_INIT(soap);
+
+#ifdef HAVE_CGSI_PLUGIN
+  int flags;
+  flags = CGSI_OPT_DISABLE_NAME_CHECK|CGSI_OPT_DELEG_FLAG;
+  soap_register_plugin_arg (soap, client_cgsi_plugin, &flags);
+#else
+#warning "Compiling without CGSI plugin support, i.e. no security"
+#endif
+
+  MV_CSTR2PSTR(req.authorizationID,authorizationID);
+  MV_CSTR2STR(req.requestToken,requestToken);
+
+  /* To send the request ... */
+  SOAP_CALL_SRM(ResumeRequest);
+  
+  RETURN(EXIT_SUCCESS);
+}
---
+++ protos/srm/2.2/n/n_srmResumeRequest.cpp
@@ -0,0 +1,131 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef DG_DIAGNOSE
+#include "diagnose/dg.h"
+#endif
+
+#include "n_srm.h"
+#include "srm2api.h"
+#include "srm_soap27.h"
+
+#include "constants.h"
+#include "i18.h"
+#include "sysdep.h"             /* BOOL, STD_BUF, ... */
+
+#include "free.h"               /* FREE(), DELETE() */
+#include "str.h"		/* dq_param() */
+
+#include <iostream>             /* std::string, cout, endl, ... */
+#include <sstream>              /* ostringstream */
+
+using namespace std;
+
+/*
+ * srmResumeRequest request constuctor
+ */
+srmResumeRequest::srmResumeRequest()
+{
+  init();
+}
+
+/*
+ * Initialise srmResumeRequest request
+ */
+void
+srmResumeRequest::init()
+{
+  /* request (parser/API) */
+  requestToken = NULL;
+
+  /* response (parser) */
+
+}
+
+/*
+ * srmResumeRequest request copy constuctor
+ */
+srmResumeRequest::srmResumeRequest(Node &node)
+{
+  init();
+  Node::init(node);
+}
+
+/*
+ * srmResumeRequest request destructor
+ */
+srmResumeRequest::~srmResumeRequest()
+{
+  DM_DBG_I;
+
+  /* request (parser/API) */
+  DELETE(requestToken);
+
+  /* response (parser) */
+  
+  DM_DBG_O;
+}
+
+/*
+ * Free process-related structures.
+ */
+void
+srmResumeRequest::finish(Process *proc)
+{
+  DM_DBG_I;
+
+  FREE_SRM_RET(ResumeRequest);
+}
+
+int
+srmResumeRequest::exec(Process *proc)
+{
+  DM_DBG_I;
+
+#ifdef SRM2_CALL
+  NEW_SRM_RET(ResumeRequest);
+
+  ResumeRequest(
+    soap,
+    EVAL2CSTR(srm_endpoint),
+    EVAL2CSTR(authorizationID),
+    EVAL2CSTR(requestToken),
+    resp
+  );
+#endif
+
+  /* matching */
+  if(!resp || !resp->srmResumeRequestResponse) {
+    DM_LOG(DM_N(1), "no SRM response\n");
+    RETURN(ERR_ERR);
+  }
+
+  RETURN(matchReturnStatus(resp->srmResumeRequestResponse->returnStatus, proc));
+}
+
+std::string
+srmResumeRequest::toString(Process *proc)
+{
+  DM_DBG_I;
+
+  GET_SRM_RESP(ResumeRequest);
+  BOOL quote = TRUE;
+  std::stringstream ss;
+
+  /* request */  
+  SS_SRM("srmResumeRequest");
+  SS_P_DQ(authorizationID);
+  SS_P_DQ(requestToken);
+
+  /* response (parser) */
+  SS_P_DQ(returnStatus.explanation);
+  SS_P_DQ(returnStatus.statusCode);
+
+  /* response (API) */
+  if(!resp || !resp->srmResumeRequestResponse) RETURN(ss.str());
+
+  SS_P_SRM_RETSTAT(resp->srmResumeRequestResponse);
+
+  RETURN(ss.str());
+}

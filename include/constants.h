b243f4c61fad3b60675800df0a18a7448837975e
Paul Millar <paul.millar@desy.de>
2016-02-20 09:25:48 +0100

    Compilation: make defining integer constants conditional

--- include/constants.h
+++ include/constants.h
@@ -10,23 +10,57 @@
 #define ERR_SYSTEM      5                       /* system error (malloc failed, etc.) */
 
 /* limits ((U)INT_MAX/MIN defined in limits.h) */
+#ifndef UINT_MIN
 #define UINT_MIN	0U
+#endif
+#ifndef INT8_MAX
 #define INT8_MAX	127
+#endif
+#ifndef INT8_MIN
 #define INT8_MIN	(-INT8_MAX - 1)
+#endif
+#ifndef UINT8_MAX
 #define UINT8_MAX	255U
+#endif
+#ifndef UINT8_MIN
 #define UINT8_MIN	0U
+#endif
+#ifndef INT16_MAX
 #define INT16_MAX	32767
+#endif
+#ifndef INT16_MIN
 #define INT16_MIN	(-INT16_MAX - 1)
+#endif
+#ifndef UINT16_MAX
 #define UINT16_MAX	65535U
+#endif
+#ifndef UINT16_MIN
 #define UINT16_MIN	0U
+#endif
+#ifndef INT32_MAX
 #define INT32_MAX	2147483647
+#endif
+#ifndef INT32_MIN
 #define INT32_MIN	(-INT32_MAX - 1)
+#endif
+#ifndef UINT32_MAX
 #define UINT32_MAX	4294967295U
+#endif
+#ifndef UINT32_MIN
 #define UINT32_MIN	0U
+#endif
+#ifndef INT64_MAX
 #define INT64_MAX	9223372036854775807LL
+#endif
+#ifndef INT64_MIN
 #define INT64_MIN	(-INT64_MAX - 1LL)
+#endif
+#ifndef UINT64_MAX
 #define UINT64_MAX	18446744073709551615ULL
+#endif
+#ifndef UINT64_MIN
 #define UINT64_MIN	0ULL
+#endif
 
 /* S2 specific constants */
 #define S2_UNDEF_STR    "(undef)"	/* undefined */
